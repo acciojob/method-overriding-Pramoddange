9475a8b16acc03777bc0d4e62d6071005395500f
Pramod Dange <pramodsdange1@gmail.com>
2024-01-18 18:16:56 +0530

    resolve error

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,13 +10,18 @@ public class Main {
 
     }
     public class B extends A{
+        @Override
         public String meth(){
             return"Method is overridden in Extendend class B";
         }
     }
     public void main(String[]args){
-        B b=new B();
-        b.meth();
+        B objB = new B();
+        String resultTask3 = objB.meth();
+        System.out.println(resultTask3);
 
+        // Task 5
+        String resultTask5 = objB.meth();
+        System.out.println(resultTask5);
     }
 }
